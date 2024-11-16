9e726aa8d811d14f88e21bae4cc404da150f2190
Steve Shreeve <steve.shreeve@gmail.com>
2024-11-15 23:12:56 -0700

    Simpler timer mechanism

--- picousb.c
+++ picousb.c
@@ -1,5 +1,14 @@
 #include "picousb.h"
 
+// ==[ Manage timer ticks ]==
+
+volatile uint32_t timer_ticks = 0;
+
+bool timer_callback(struct repeating_timer *t) {
+    timer_ticks++;
+    return true; // Keep the timer running
+}
+
 char *buf = (char[1024]) { 0 };
 
 void poll_ep1_in(void *arg) {
@@ -13,13 +22,18 @@ int main() {
     usb_debug(1);
     usb_init();
 
+    // Create a repeating timer
+    struct repeating_timer timer;
+    add_repeating_timer_ms(300, timer_callback, NULL, &timer);
+
+    uint32_t last_ticks = 0;
+
     while (1) {
         usb_task();
 
         if (devices[1].state == DEVICE_READY) {
-            static uint64_t last_attempt = 0;
-            if(!last_attempt || ((time_us_64() - last_attempt) > 400000)) {
-                last_attempt = time_us_64();
+            if (last_ticks != timer_ticks) {
+                last_ticks  = timer_ticks;
                 queue_callback(poll_ep1_in, NULL);
             }
         }
