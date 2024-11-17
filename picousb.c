1f80219f2bd2a4c99d7a45551e89f648353f1900
Steve Shreeve <steve.shreeve@gmail.com>
2024-11-16 23:35:41 -0700

    Add some goofy code, remove soon

--- picousb.c
+++ picousb.c
@@ -20,13 +20,21 @@ void poll_ep1_in(void *arg) {
     bulk_transfer(ep, buf, len);
 }
 
+void enquire_ep2_out(void *arg) {
+    endpoint_t *ep = &eps[2];
+    buf[0] = 0x06; // ASTM enquire
+    uint16_t len = 1;
+
+    bulk_transfer(ep, buf, len);
+}
+
 int main() {
     usb_debug(1);
     usb_init();
 
     // Create a repeating timer
     struct repeating_timer timer;
-    add_repeating_timer_ms(300, timer_callback, NULL, &timer);
+    add_repeating_timer_ms(2000, timer_callback, NULL, &timer);
 
     uint32_t last_ticks = 0;
 
@@ -36,7 +44,11 @@ int main() {
         if (devices[1].state == DEVICE_READY) {
             if (last_ticks != timer_ticks) {
                 last_ticks  = timer_ticks;
-                queue_callback(poll_ep1_in, NULL);
+                if (last_ticks % 10) {
+                    queue_callback(poll_ep1_in, NULL);
+                } else {
+                    queue_callback(enquire_ep2_out, NULL);
+                }
             }
         }
     }
