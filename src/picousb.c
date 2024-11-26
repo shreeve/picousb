29a2f98280040751646f6d388e9049c458c6714e
Steve Shreeve <steve.shreeve@gmail.com>
2024-11-26 07:10:41 -0700

    Line up

--- src/picousb.c
+++ src/picousb.c
@@ -110,9 +110,9 @@ void setup_pipe(pipe_t *pp, uint8_t phe, usb_endpoint_descriptor_t *epd,
     *pp->ecr = EP_CTRL_ENABLE_BITS             // Enable this endpoint
              |  (phe ? SINGLE_BUFFER           // Non-epx are single buffered
                      : DOUBLE_BUFFER)          // And epx starts double buffered
-             |  (pp->type << 26)               // Set transfer type
-             |(((pp->interval || 1) - 1) << 16) // Polling interval minus 1 ms
-             |(((uint32_t) pp->buf) & 0xfc0);   // DSPRAM offset: 64-byte aligned
+             |   pp->type                << 26 // Set transfer type
+             | ((pp->interval || 1) - 1) << 16 // Polling interval minus 1 ms
+             |(((uint32_t) pp->buf) & 0xfc0);  // DSPRAM offset: 64-byte aligned
 
     // NOTE: Endpoints should start with DATA0. However, there are some devices
     // that are non-standard and start with DATA1. Linux just hard codes these
