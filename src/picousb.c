7e897f32074414b06d07b98c0f1d10bb2a6652ad
Steve Shreeve <steve.shreeve@gmail.com>
2024-12-06 17:02:09 -0700

    Prepare for drivers

--- src/picousb.c
+++ src/picousb.c
@@ -729,31 +729,31 @@ bool enumerate_descriptors(void *ptr, device_t *dev) {
 
                 // Special case: CDC/ACM uses two interfaces (control and data)
                 if (ias                     == 1    && // If only 1 interface,
-                    ifd->bInterfaceClass    == 0x02 && // but, we're a CDC class
-                    ifd->bInterfaceSubClass == 0x02)   // and ACM subclass, then
-                    ias = 2;                           // require 2 interfaces
+                    ifd->bInterfaceClass    == 0x02 && // of CDC class, and
+                    ifd->bInterfaceSubClass == 0x02)   // of ACM subclass,
+                    ias = 2;                           // it uses 2 interfaces
 
-                // // Try to find a driver for this interface
-                // uint8_t i;
-                // for (i = 0; i < driver_count; i++) {
-                //     const driver_t *driver = &drivers[i];
-                //
-                //     if (driver->open(dev, ifd)) {
-                //         debug("%s driver opened\n", driver->name);
-                //
-                //         // // Bind each interface association to the driver
-                //         // for (uint8_t j = 0; j < ias; j++) {
-                //         //     uint8_t k = ifd->bInterfaceNumber + j;
-                //         //     dev->itf2drv[k] = i; // TODO: This needs to start with an invalid value
-                //         // }
-                //         //
-                //         // // Bind all endpoints to the driver
-                //         // endpoint_bind_driver(dev->ep2drv, ifd, len, i);
-                //
-                //         break;
-                //     }
-                // }
-                // if (i == driver_count) debug("No matching driver found");
+                // Find a driver for this interface
+                uint8_t i;
+                for (i = 0; i < driver_count; i++) {
+                    const driver_t *driver = &drivers[i];
+
+                    if (driver->open(dev, ifd)) {
+                        debug("Trying the %s driver\n", driver->name);
+
+                        // Bind each interface association to the driver
+                        // for (uint8_t j = 0; j < ias; j++) {
+                        //     uint8_t k = ifd->bInterfaceNumber + j;
+                        //     dev->itf2drv[k] = i; // TODO: This needs to start with an invalid value
+                        // }
+                        //
+                        // Bind all endpoints to the driver
+                        // endpoint_bind_driver(dev->ep2drv, ifd, len, i);
+
+                        break;
+                    }
+                }
+                if (i == driver_count) debug("No matching driver found");
             }   break;
 
             case USB_DT_ENDPOINT:
